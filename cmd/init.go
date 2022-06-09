434075df0901f853ddab7279d26ea98be4fad8fc
lunz1207 <lunz1207@gmail.com>
2022-06-09 20:45:09 +0800

    feat: cli

---
+++ cmd/init.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"flag"
+
+	"path/filepath"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+func deploy(cmd *cobra.Command, args []string) {
+
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset.AppsV1()
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		namespace := "default"
+		pod := "svc-demo-76788ccbc7-wnzlr"
+		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				pod, namespace, statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Delpoy tink to cluster",
+	Long:  `All software has inits. This is Hugo's`,
+	Run:   deploy,
+}
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,30 +1,30 @@
 echo "------------------- uninstall start --------------------"
 echo "------------------- uninstall influxdb --------------------"
-helm uninstall influxdb 
+helm uninstall influxdb
 
-echo "------------------- uninstall grafana --------------------"
-helm uninstall grafana 
+# echo "------------------- uninstall grafana --------------------"
+# helm uninstall grafana
 
-echo "------------------- uninstall nfs --------------------"
-helm uninstall svc 
+echo "------------------- uninstall demo service --------------------"
+kubectl delete -f https://raw.githubusercontent.com/ante-involutum/demo/main/deployment.yaml
 
-echo "------------------- uninstall api service --------------------"
-kubectl delete -f service/demo/deployment.yaml
-
-echo "------------------- uninstall jmeter cluster --------------------"
-kubectl delete -f jmeter/deployment.yaml
+# echo "------------------- uninstall jmeter cluster --------------------"
+# kubectl delete -f jmeter/deployment.yaml
 
 echo "------------------- uninstall files service --------------------"
-kubectl delete -f service/files/deployment.yaml
+kubectl delete -f https://raw.githubusercontent.com/ante-involutum/files/main/deployment.yaml
 
-echo "------------------- uninstall controller service --------------------"
-kubectl delete -f service/controller/deployment.yaml
+echo "------------------- uninstall tink service --------------------"
+kubectl delete -f https://raw.githubusercontent.com/ante-involutum/tink/main/deployment.yaml
 
-echo "------------------- uninstall pvc --------------------"
-# kubectl delete -f deployment/nfs-pvc.yaml
+echo "------------------- uninstall nfs --------------------"
+helm uninstall svc
 
 echo "------------------- uninstall RBAC --------------------"
 kubectl delete -f deployment/RBAC.yaml
 
-echo "------------------- uninstall done --------------------"
+# echo "------------------- uninstall pvc --------------------"
+# kubectl delete -f deployment/nfs-pvc.yaml
 
+
+echo "------------------- uninstall done --------------------"
